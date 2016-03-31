ce8de0a6dfb9719c8abae8413b135d9342d02be9
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-03-31 01:25:45 -0400

    add export JSR308 env var and JAVA_HOME to let run-checker.sh works correct when the server mount to apache2

--- run-checker.sh
+++ run-checker.sh
@@ -8,6 +8,10 @@
 # echo doesn't work here since it interprets '\n' and other chars
 #
 # TODO: use -Xmx512m if we need more memory
+
+#tricky, currently need hard code JAVA_HOME and JSR308 path here in order to let this script work normally in Apache wsgi mode.
+JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
+JSR308=/home/charleszhuochen/jsr308
 cat <<ENDEND | $JAVA_HOME/bin/java -Xmx2500M -Xbootclasspath/p:$JSR308/checker-framework/checker/dist/javac.jar -ea -ea:com.sun.tools... -cp $JSR308/checker-framework/checker/dist/checker.jar:CheckerPrinter/bin:CheckerPrinter/javax.json-1.0.jar:$JAVA_HOME/lib/tools.jar checkerprinter.InMemory $JSR308
 $1
 ENDEND
