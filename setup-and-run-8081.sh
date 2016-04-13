c4ea17ed1b6b6e8b55e547a8754f22878ad807e3
Zhuo Chen - ECE <z359chen@eisop.uwaterloo.ca>
2016-04-13 11:36:07 -0400

    add log configuration in 8081 setup-server script

--- setup-and-run-8081.sh
+++ setup-and-run-8081.sh
@@ -2,5 +2,7 @@
 #This file need to be run AFTER deploy script
 
 if [ ! -d "config-8081" ] ; then mkdir config-8081 ; fi
-mod_wsgi-express setup-server --port 8081 wsgi-scripts/checkerweb.wsgi --server-root=./config-8081
+if [ ! -d "log-8081" ] ; then mkdir log-8081 ; fi
+
+mod_wsgi-express setup-server --port 8081 wsgi-scripts/checkerweb.wsgi --server-root=./config-8081 --log-directory ./log-8081 --error-log-name error.log --access-log-name acess.log 
 ./config-8081/apachectl start
