fdf1abe2afd0a23178c4012b88d7bd2a21e18692
Zhuo Chen - ECE <z359chen@eisop.uwaterloo.ca>
2016-04-15 15:14:00 -0400

    fix sliently killing git pull. but not sure why this works.

--- build-checker-framework.sh
+++ build-checker-framework.sh
@@ -7,22 +7,23 @@ if [ ! -d "jsr308" ] ; then
     mkdir ./jsr308;
 fi 
 
-cd ./jsr308
-
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
-export SHELLOPTS
+# export SHELLOPTS
+
+cd ./jsr308
 
 ## Build Checker Framework
 if [ -d ./checker-framework ] ; then
     # Older versions of git don't support the -C command-line option
+    # echo "do pull"
     (cd ./checker-framework && git pull)
 else
     echo cloning
     git clone https://github.com/typetools/checker-framework.git
 fi
 
-echo here
+# echo here
 
 # This also builds annotation-tools and jsr308-langtools
 cd checker-framework/
