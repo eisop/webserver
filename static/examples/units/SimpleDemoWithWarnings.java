8f750b880b5460aa7200b948d12113b117b6c956
Jeff Luo <j36luo@uwaterloo.ca>
2016-04-15 03:43:51 -0400

    Updated Units Demos

--- static/examples/units/SimpleDemoWithWarnings.java
+++ static/examples/units/SimpleDemoWithWarnings.java
@@ -1,20 +1,30 @@
 import org.checkerframework.checker.units.qual.*;
 import org.checkerframework.checker.units.UnitsTools;
+import static org.checkerframework.checker.units.UnitsTools.m;
 
-// An simple example of dividing meters by seconds
+// An example of declaring variables and values with units
 public class SimpleDemoWithWarnings {
-  void demo1() {
+  void demo() {
+    // to declare a variable with a unit, annotate the type of the variable with a units qualifier:
+    @m int x;
+
+    // to assign a unit to a number, multiply a number by an appropriate unit constant from UnitsTools;
+    x = 5 * UnitsTools.m;
+
+    // with static import of the unit constant, the syntax can be simplified further:
+    x = 5 * m; 
+
     @m int meters = 5 * UnitsTools.m;
     @s int seconds = 2 * UnitsTools.s;
-    
+
     @mPERs int speed = meters / seconds;
-    
+
     // addition and subtraction between meters
     // and seconds do not make sense scientifically
     // so an error is produced here
     //:: error: (assignment.type.incompatible)
-    @m int x = meters + seconds;
+    @m int foo = meters + seconds;
     //:: error: (assignment.type.incompatible)
-    @s int y = seconds - meters;
+    @s int bar = seconds - meters;
   }
 }
