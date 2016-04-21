855d04aa9cae86859c1c18faf12d0cc0b10b8ecf
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-04-21 14:10:25 -0400

    fix relative path issues in setup-and-run-8081.sh

--- shell-scripts/setup-and-run-8081.sh
+++ shell-scripts/setup-and-run-8081.sh
@@ -10,15 +10,15 @@ if [ ! -d "../jsr308" ] ; then
 fi
 
 if [ ! -d "../config-8081" ] ; then
-    mkdir config-8081 ;
+    mkdir ../config-8081 ;
 fi
 
 if [ ! -d "../log-8081" ] ; then
-    mkdir log-8081 ;
+    mkdir ../log-8081 ;
 fi
 
 mod_wsgi-express setup-server \
-		 --port 8081 wsgi-scripts/checkerweb.wsgi \
+		 --port 8081 ../wsgi-scripts/checkerweb.wsgi \
 		 --server-root=../config-8081 \
 		 --log-directory ../log-8081 \
 		 --error-log-name error.log \
