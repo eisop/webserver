14f7f21947bd9b7108b9857c7fef9a8f22fe5521
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-02 18:45:51 -0700

    base version of devlop rise4fun integration. add multiple url to wsgi mapping in apache config file, add cfPath and isRise4Fun in botter_server.py to indicate using which checker framework and using which printer to print output

--- shell-scripts/run-checker.sh
+++ shell-scripts/run-checker.sh
@@ -16,8 +16,10 @@ cd $(dirname "$0")
 # order to let this script work normally in Apache wsgi mode.
 JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
 
-CF=$(cd ../enabled-checker-framework && pwd)
+# CF=$(cd ../enabled-checker-framework && pwd)
+CF=$2
+IS_RISE4FUN=$3
 
-cat <<ENDEND | $JAVA_HOME/bin/java -Xmx2500M -Xbootclasspath/p:$CF/checker/dist/javac.jar -ea -ea:com.sun.tools... -cp $CF/checker/dist/checker.jar:../CheckerPrinter/bin:../CheckerPrinter/javax.json-1.0.jar:$JAVA_HOME/lib/tools.jar checkerprinter.InMemory $CF
+cat <<ENDEND | $JAVA_HOME/bin/java -Xmx2500M -Xbootclasspath/p:$CF/checker/dist/javac.jar -ea -ea:com.sun.tools... -cp $CF/checker/dist/checker.jar:../CheckerPrinter/bin:../CheckerPrinter/javax.json-1.0.jar:$JAVA_HOME/lib/tools.jar checkerprinter.InMemory $CF $IS_RISE4FUN
 $1
 ENDEND
