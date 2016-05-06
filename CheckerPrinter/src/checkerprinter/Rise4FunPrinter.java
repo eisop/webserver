dcc04a024d1b5490c115ae8cd260eead63787c36
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-05-06 11:14:16 -0400

    Update CheckerPrinter: base version of developing Rise4FunPrinter

---
+++ CheckerPrinter/src/checkerprinter/Rise4FunPrinter.java
@@ -0,0 +1,29 @@
+package checkerprinter;
+
+import java.util.List;
+
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+
+public class Rise4FunPrinter extends Printer {
+
+    @Override
+    public void printException(String msg) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void printSuccess() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void printDiagnosticReport(
+            List<Diagnostic<? extends JavaFileObject>> diagnosticList) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
