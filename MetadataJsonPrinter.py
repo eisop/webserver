090134fa3443bc1c4c01b9b6d0eb97f74c96eabb
fernando <fernandopena961226@gmail.com>
2016-12-23 19:07:36 -0500

    Change MetadataJsonPriter.py to use the modele os.path since multiple operating systems might use different separators. In addition I also changed Metadatainfo.json. Instead of having a hard coded path to the file. It will contain the checker name and the name of the file.

--- MetadataJsonPrinter.py
+++ MetadataJsonPrinter.py
@@ -1,27 +1,37 @@
-# The purpose of this module is to return the metadata information in a json format or to get a specific part of information of the metadata.
-# we can get the metadata as a json format by calling the function getMetadata
-# additionally we can get any specific information of the metadata by calling the method getSpecific and passing the name of the value you want to get.
+# The purpose of this module is to return the metadata information in a json format or to
+# get a specific part of information of the metadata.we can get the metadata as a json format
+# by calling the function getMetadata additionally we can get any specific information of the
+#  metadata by calling the method getSpecific and passing the name of the value you want to get.
 # Example getSpecific('Name') return Checker_Framework.
 
 import json
-def getMetaData():
+import os.path
+#global constants
+#ROOT_DIR = connanical path of the directory where thsi file is locaded it.
+#EXAMPLE_DIR = connanical path of the examples directory.
+#META_DATA_INFO_PATH = connanical path of the examples directory.
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+EXAMPLE_DIR = os.path.join(ROOT_DIR, "static",'examples')
+META_DATA_INFO_PATH= os.path.join(ROOT_DIR, 'MetadataInfo.json')
 
-    
+print(EXAMPLE_DIR)
+
+def getMetaData():
     # dump the data inside MetadataInfo.json into data
-    with open('MetadataInfo.json','r') as f:
+    with open(META_DATA_INFO_PATH,'r') as f:
         data = json.load(f)
 
-    samples=[]
-
-
+    samples = []
     # gets the code inside the samples and created a dictornary containing the Name of the sample and it source code
     # run this loop for each dictionary inside data['Samples']
     for sample in data['Samples']:
+        # get the path of the sample by using the os.path.join() function and the CheCheckerName and FileName.
+        # which are both contained inside Samples
+        sample_path = os.path.join(EXAMPLE_DIR, sample['CheckerName'], sample['FileName'])
 
 
-        # the path of sample = sample['Path']
         # input the source of sample into x
-        with open(sample['Path'],'r') as x:
+        with open(sample_path,'r') as x:
 
             # create a dictonary with the key = Name, value = sample['Name'] and key = Source, value =  x.read()
             sample_dict={"Name":sample['Name'],"Source":x.read()}
@@ -59,11 +69,9 @@ def getMetaData():
 
 def getSpecific( key ):
     # dump all the inforaton of MetadataInfo into data
-    with open('MetadataInfo.json','r') as f:
+    with open(META_DATA_INFO_PATH,'r') as f:
         data = json.load(f)
         #return the a value of metadata depending on its key. Example if key = 'Name' the return value is Checker_Framework.
         return data[key]
-
-
 
 
