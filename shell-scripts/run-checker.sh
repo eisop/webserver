4f11c3218c2f09cb33a2461ce0743071de0e0a14
Werner Dietl <wdietl@gmail.com>
2024-06-19 16:45:45 -0400

    Remove more tools.jar usages

--- shell-scripts/run-checker.sh
+++ shell-scripts/run-checker.sh
@@ -22,6 +22,6 @@ fi
 CF=$2
 IS_RISE4FUN=$3
 
-cat <<ENDEND | $JAVA_HOME/bin/java -Xmx2500M -ea -ea:com.sun.tools... -cp $CF/checker/dist/checker.jar:$CF/checker/dist/javac.jar:../CheckerPrinter/bin:../CheckerPrinter/javax.json-1.0.jar:$JAVA_HOME/lib/tools.jar checkerprinter.InMemory $CF $IS_RISE4FUN
+cat <<ENDEND | $JAVA_HOME/bin/java -Xmx2500M -ea -ea:com.sun.tools... -cp $CF/checker/dist/checker.jar:$CF/checker/dist/javac.jar:../CheckerPrinter/bin:../CheckerPrinter/javax.json-1.0.jar checkerprinter.InMemory $CF $IS_RISE4FUN
 $1
 ENDEND
